facdbd98e2dcd8789147bde5c48738c05bce83d6
researchersec <109282836+researchersec@users.noreply.github.com>
2024-05-07 00:26:03 +0200

    Update watch_stream.py

--- watch_stream.py
+++ watch_stream.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import time  # Import the time module for the sleep function
 
 def watch_stream():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Runs Chrome in headless mode.
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--mute-audio")  # Mutes the stream to avoid noise during automation
+    chrome_options.add_argument("--mute-audio")  # Mutes the stream
 
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service(executable_path=r'/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
     driver.get("https://www.twitch.tv/angelito20163")
-    # Keep the browser open for a certain period
-    driver.implicitly_wait(320) 
+    time.sleep(300)  # Wait for 5 minutes (300 seconds)
     driver.quit()
 
 if __name__ == "__main__":
